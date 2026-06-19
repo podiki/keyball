8685363ebce327058b776a83b966a67e1e816c47
podiki <podiki@users.noreply.github.com>
2026-06-19 17:40:40 -0400

    Update mouse button keycodes (qmk v0.30 change)

--- qmk_firmware/keyboards/keyball/one47/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/one47/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_right_ball(
     SSNP_FRE , _______  , KC_7     , KC_8     , KC_9     , _______  ,                            _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , _______  ,
-    SSNP_VRT , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , _______  ,
+    SSNP_VRT , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            KC_PGUP  , MS_BTN1  , KC_DOWN  , MS_BTN2  , MS_BTN3  , _______  ,
     SSNP_HOR , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,      S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
                _______  , KC_0     , KC_DOT   , _______  , _______  ,   _______  ,  KC_DEL     , _______  ,                                  _______
   ),
